3ab35af37ccbad0d35cf4197922c97933133df32
Parman Sangha <105144305+Parman-Sangha@users.noreply.github.com>
2024-09-09 22:49:40 -0700

    update

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Parman Sangha</h1>
+      <Link
+        href="https://github.com/Parman-Sangha?tab=repositories"
+        target="_blank"
+      >
+        Github Repository
+      </Link>
+    </div>
+  );
+}
